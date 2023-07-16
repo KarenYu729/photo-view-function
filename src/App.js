d4a60da041378ca89bfec2703337c8bae5562911
Jiahui Yu <97644710+KarenYu729@users.noreply.github.com>
2023-07-15 21:38:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {Container} from "react-bootstrap";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home";
+import {Nature} from "./pages/Nature";
+import {Creature} from "./pages/Creature";
+import {NavBarComponent} from "./components/NavBar.component";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <NavBarComponent/>
+                <Container className={"mb-4"}>
+                    <Routes>
+                        <Route path={'/photo-view-function/'} element={<Home/>}></Route>
+                        <Route path={'/photo-view-function/nature'} element={<Nature/>}></Route>
+                        <Route path={'/photo-view-function/creature'} element={<Creature/>}></Route>
+                    </Routes>
+                </Container>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
